626e26f71b5e50925ad8236ee0747310999caec9
athallarafly <athallaraflymn@gmail.com>
2022-12-28 00:34:27 +0700

    error cant show project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import 'assets/scss/style.scss';
+import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      hai dunia!
+      <Router>
+        <Routes>
+          <Route path="/" element={LandingPage}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
